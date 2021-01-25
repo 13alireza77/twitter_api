f072bcaaf60c0e59590dbb09c8bf8c82cce5cb2b
user <http://185.208.77.246/google/image_crawler.git>
2021-01-25 10:08:37 +0330

    add twitt

---
+++ twitt/models.py
@@ -0,0 +1,22 @@
+from django.core.validators import RegexValidator
+from django.db import models
+from prof.models import UserProfile
+
+
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
+
+
+class Twitt(models.Model):
+    user = models.ForeignKey("UserProfile", on_delete=models.CASCADE)
+    text = models.CharField(max_length=250, blank=True, null=True)
+    image = models.ImageField(blank=True, null=True)
+    video = models.CharField(blank=True, null=True, upload_to=user_directory_path)
+    date = models.DateField(auto_now_add=True, auto_created=True)
+    deleted = models.BooleanField(default=False)
+
+
+class hashtag(models.Model):
+    name = models.CharField(validators=RegexValidator(r"^(#\w+\s+)+$"), unique=True)
+    occurrences = models.IntegerField(default=0, null=False, blank=False, auto_created=True)
